35d52b9ae5e450cd55541721a15059dd6a5fc411
Osama-14 <mohdosama42000@gmail.com>
2022-10-25 20:00:54 +0500

    updated delivery btn

--- components/HeaderTabs.js
+++ components/HeaderTabs.js
@@ -1,13 +1,39 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {View, Text, TouchableOpacity} from 'react-native';
+import React, {useState} from 'react';
 
 export default function HeaderTabs() {
+  const [activeTab, setActiveTab] = useState('Delivery');
+
   return (
-    <View>
-      <Headerbutton />
-      {/* headerbutton */}
+    <View style={{flexDirection: 'row', alignSelf: 'center'}}>
+      <Headerbutton
+       text="Delivery" 
+       btnColor="black"
+        textColor="white"
+        activeTab={activeTab}
+        setActiveTab={setActiveTab}
+        
+        />
+      <Headerbutton text="Delivery" btnColor="white" textColor="black" />
     </View>
   );
 }
 
-const Headerbutton = () => <Text>Delivery</Text>;
+const Headerbutton = props => (
+  <TouchableOpacity
+    style={{
+      backgroundColor: props.btnColor,
+      paddingVertical: 6,
+      paddingHorizontal: 16,
+      borderRadius: 30,
+    }}>
+    <Text
+      style={{
+        color: props.textColor,
+        fontSize: 15,
+        fontWeight: '900',
+      }}>
+      {props.text}
+    </Text>
+  </TouchableOpacity>
+);
