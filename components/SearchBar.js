d4de0d2b238d16f6df4c92027edf3d74b8795bde
Osama-14 <mohdosama42000@gmail.com>
2022-10-31 19:56:52 +0500

    location

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,6 +1,9 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
+import Ionicons  from "react-native-vector-icons/Ionicons"
+// import Loction from 'react-native-ionicons'
+
 
 export default function SearchBar() {
   return (
@@ -13,9 +16,25 @@ export default function SearchBar() {
             borderRadius: 20,
             fontWeight:"700",
             marginTop: 7,
-        }
+        },
+        textInputContainer:{
+            backgroundColor:"#eee",
+            borderRadius:50,
+            flexDirection:"row",
+            alignItems:"center",
+            marginRight: 10,
+        },
       }}
-       />
+
+      renderLeftButton={() => (
+        <View>
+            {/* <Location name="location" /> */}
+            {/* <ion-icon name="location-sharp"></ion-icon> */}
+            <Ionicons name="location-sharp" size={24}/>
+        </View>
+      )}
+
+   />
     </View>
   )
 }
